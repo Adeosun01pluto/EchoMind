aa7be156f854d408c0749632b6439b0389597569
Adeosun01pluto <adeosun01pluto@gmail.com>
2023-09-28 22:55:32 -0700

    ads

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,3 +1,6 @@
 // export const BASE_URL = "http://localhost:4001"
 export const BASE_URL = "https://echo-server-8cvm.onrender.com"
 // export const online_url= "https://echo-swart.vercel.app/"
+
+// export const Ads = <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2148071257659578"
+// crossorigin="anonymous"></script>
