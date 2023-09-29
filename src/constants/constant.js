d1fc67979aafbbe29290f3d403ca84ac4217c035
Adeosun01pluto <adeosun01pluto@gmail.com>
2023-09-28 22:39:40 -0700

    base url

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:4001"
-// export const BASE_URL = "https://echo-server-8cvm.onrender.com"
+// export const BASE_URL = "http://localhost:4001"
+export const BASE_URL = "https://echo-server-8cvm.onrender.com"
 // export const online_url= "https://echo-swart.vercel.app/"
