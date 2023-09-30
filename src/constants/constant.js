82a2b819c524e884b3fcee08de41a96c70088c00
Adeosun01pluto <adeosun01pluto@gmail.com>
2023-09-30 12:26:39 -0700

    base url

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:4001"
-// export const BASE_URL = "https://echo-server-8cvm.onrender.com"
+// export const BASE_URL = "http://localhost:4001"
+export const BASE_URL = "https://echo-server-8cvm.onrender.com"
 
 // export const Ads = <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2148071257659578"
 // crossorigin="anonymous"></script>
